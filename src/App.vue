31f3a5e37eb71ee180a0a4936690889230cfae70
paiizz <wanchanapon.t@ku.th>
2016-11-24 17:19:11 +0700

    initialize git page and create header

---
+++ src/App.vue
@@ -0,0 +1,52 @@
+<template>
+  <div id="app">
+    <Header></Header>
+    <Body></Body>
+  </div>
+</template>
+
+<script>
+import Header from './components/Header.vue'
+import Body from './components/Body.vue'
+export default {
+  name: 'app',
+  data () {
+    return {
+      renderer : null
+    }
+  },
+  mounted(){},
+  components : {
+    Header,
+    Body
+  }
+}
+</script>
+
+<style scoped>
+#app {
+  font-famly: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,74 @@
+<template lang="html">
+  <section class="hero is-brown">
+    <div class="columns">
+      <div class="column is-one-quarter">
+        <div class="notification is-brown photo">
+          <a href="https://www.facebook.com/paiizz.mpk.1">
+            <img src="src/assets/profile.png">
+          </a>
+        </div>
+      </div>
+      <div class="column">
+        <div class="notification is-brown">
+          <div class="hero-head">
+            <div class="title-1 my-title text-white">
+              Wanchanapon Thanwaranurak
+            </div>
+            <div class="title-2 my-title text-white sub-head">
+              Software Engineer
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  data () {
+    return {}
+  },
+  computed: {},
+  mounted () {},
+  methods: {},
+  components: {
+  }
+}
+</script>
+
+<style scoped>
+  .my-title {
+    text-align: center;
+  }
+  
+  .title-1 {
+    font-size: 44px;
+  }
+  
+  .title-2 {
+    font-size: 32px;
+  }
+  
+  .head {
+    padding-top: 3%;
+    padding-bottom: 1%;
+  }
+  
+  .sub-head {
+    padding-top: 1%;
+  }
+  
+  .is-brown {
+    background-color: #302723;
+  }
+  
+  .text-white {
+    color: white;
+  }
+  
+  .photo {
+    margin-left: 15%;
+    width: 250px;
+  }
+</style>
